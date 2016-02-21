be19f3193606c817afeab1c1e430a6085db3a8b0
acenosekai <acenosekai@gmail.com>
2016-02-21 19:03:24 +0700

    base repository

---
+++ app/src/main/java/com/acenosekai/antplayer/ant/AntRepo.java
@@ -0,0 +1,15 @@
+package com.acenosekai.antplayer.ant;
+
+import io.realm.Realm;
+
+/**
+ * Created by Acenosekai on 2/13/2016.
+ * Rock On
+ */
+public class AntRepo {
+    protected Realm r;
+
+    public AntRepo(Realm r) {
+        this.r = r;
+    }
+}

3cdaaf9a9cd439c4218fd302e61cfb4cba2cc23b
acenosekai <acenosekai@gmail.com>
2016-02-27 18:12:27 +0700

    update bass library

--- app/src/main/java/com/un4seen/bass/BASSWV.java
+++ app/src/main/java/com/un4seen/bass/BASSWV.java
@@ -10,18 +10,18 @@ package com.un4seen.bass;
 import java.nio.ByteBuffer;
 
 public class BASSWV {
-    // BASS_CHANNELINFO type
-    public static final int BASS_CTYPE_STREAM_WV = 0x10500;
+	// BASS_CHANNELINFO type
+	public static final int BASS_CTYPE_STREAM_WV = 0x10500;
 
     static {
         System.loadLibrary("basswv");
     }
 
-    public static native int BASS_WV_StreamCreateFile(String file, long offset, long length, int flags);
+	public static native int BASS_WV_StreamCreateFile(String file, long offset, long length, int flags);
 
-    public static native int BASS_WV_StreamCreateFile(ByteBuffer file, long offset, long length, int flags);
+	public static native int BASS_WV_StreamCreateFile(ByteBuffer file, long offset, long length, int flags);
 
-    public static native int BASS_WV_StreamCreateURL(String url, int offset, int flags, BASS.DOWNLOADPROC proc, Object user);
+	public static native int BASS_WV_StreamCreateURL(String url, int offset, int flags, BASS.DOWNLOADPROC proc, Object user);
 
-    public static native int BASS_WV_StreamCreateFileUser(int system, int flags, BASS.BASS_FILEPROCS procs, Object user);
+	public static native int BASS_WV_StreamCreateFileUser(int system, int flags, BASS.BASS_FILEPROCS procs, Object user);
 }
--- app/src/main/java/com/un4seen/bass/TAGS.java
+++ app/src/main/java/com/un4seen/bass/TAGS.java
@@ -3,17 +3,17 @@ package com.un4seen.bass;
 import java.nio.ByteBuffer;
 
 public class TAGS {
-    static {
+	static {
         System.loadLibrary("tags");
     }
 
-    public static native String TAGS_GetLastErrorDesc();
+	public static native String TAGS_GetLastErrorDesc();
 
-    public static native String TAGS_Read(int dwHandle, String fmt);
+	public static native String TAGS_Read(int dwHandle, String fmt);
 
-    public static native String TAGS_ReadEx(int dwHandle, String fmt, int tagtype);
+	public static native String TAGS_ReadEx(int dwHandle, String fmt, int tagtype);
 
-    public static native ByteBuffer TAGS_ReadExByte(int dwHandle, String fmt, int tagtype);
+	public static native ByteBuffer TAGS_ReadExByte(int dwHandle, String fmt, int tagtype);
 
-    public static native int TAGS_GetVersion();
+	public static native int TAGS_GetVersion();
 }

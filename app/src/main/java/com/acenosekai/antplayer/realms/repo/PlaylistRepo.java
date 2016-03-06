7ba8259f70e2237b82f30f776d3734b4bdaeb2a7
acenosekai <acenosekai@gmail.com>
2016-03-06 12:28:05 +0700

    gix playlist repository

--- app/src/main/java/com/acenosekai/antplayer/realms/repo/PlaylistRepo.java
+++ app/src/main/java/com/acenosekai/antplayer/realms/repo/PlaylistRepo.java
@@ -18,6 +18,7 @@ import io.realm.Realm;
  */
 public class PlaylistRepo extends AntRepo {
     public static final String DEFAULT_PLAYLIST = "Current Playlist";
+
     public PlaylistRepo(Realm r) {
         super(r);
     }
@@ -36,14 +37,14 @@ public class PlaylistRepo extends AntRepo {
 
     public List<Music> generateMusicListShuffle(String name, Boolean reShuffle) {
         Playlist p = findPlaylistByName(name);
-        List<Music> listMusic = new ArrayList<>();
+        List<Music> listMusic = null;
 
-        if (p.getMusicListShuffle() != null && !p.getMusicListShuffle().equals("") && !reShuffle) {
-            listMusic = _generateList(p.getMusicListShuffle());
+        if (!reShuffle) {
+            listMusic = _JsonPathToMusicList(p.getMusicListShuffle());
         } else {
-            listMusic = generateMusicList(name);
-            Collections.shuffle(listMusic);
-            saveMusicListShuffle(name, listMusic);
+            saveMusicListShuffle(name);
+            return generateMusicListShuffle(name, false);
+
         }
         return listMusic;
     }
@@ -56,41 +57,22 @@ public class PlaylistRepo extends AntRepo {
         Playlist p = findPlaylistByName(name);
         List<Music> listMusic = new ArrayList<>();
         if (p.getMusicList() != null && !p.getMusicList().equals("")) {
-            listMusic = _generateList(p.getMusicList());
+            listMusic = _JsonPathToMusicList(p.getMusicList());
         }
         return listMusic;
     }
 
-    private List<Music> _generateList(String musicPathListString) {
-        List<Music> listMusic = new ArrayList<>();
-        Gson gson = new Gson();
-        List<String> musicPathList = gson.fromJson(musicPathListString, new TypeToken<List<String>>() {
-        }.getType());
-        for (String path : musicPathList) {
-            MusicRepo mr = new MusicRepo(r);
-            Music m = mr.findOne(path);
-            if (m != null) {
-                listMusic.add(m);
-            }
-        }
-
-        return listMusic;
+    public void saveMusicListShuffle() {
+        saveMusicListShuffle(DEFAULT_PLAYLIST);
     }
 
-    public void saveMusicListShuffle(List<Music> musicList) {
-        saveMusicListShuffle(DEFAULT_PLAYLIST, musicList);
-    }
 
-    public void saveMusicListShuffle(String name, List<Music> musicList) {
+    public void saveMusicListShuffle(String name) {
         Playlist p = findPlaylistByName(name);
+        List<Music> musicList = _JsonPathToMusicList(p.getMusicList());
+        Collections.shuffle(musicList);
         r.beginTransaction();
-
-        List<String> listPath = new ArrayList<>();
-        for (Music m : musicList) {
-            listPath.add(m.getPath());
-        }
-        Gson gson = new Gson();
-        String jsonPath = gson.toJson(listPath);
+        String jsonPath = _musicListToJsonPath(musicList);
         p.setMusicListShuffle(jsonPath);
         r.commitTransaction();
     }
@@ -102,16 +84,57 @@ public class PlaylistRepo extends AntRepo {
     public void saveMusicList(String name, List<Music> musicList) {
         Playlist p = findPlaylistByName(name);
         r.beginTransaction();
+        String jsonPath = _musicListToJsonPath(musicList);
+        p.setMusicList(jsonPath);
+        r.commitTransaction();
+        saveMusicListShuffle(name);
+    }
 
+    public void addToPlaylist(List<Music> musicList) {
+        addToPlaylist(DEFAULT_PLAYLIST, musicList);
+    }
+
+    public void addToPlaylist(String name, List<Music> musicList) {
+        //repack music list to avoid realm error
+        List<Music> newMusicList = new ArrayList<>();
+        for (Music m : musicList) {
+            newMusicList.add(m);
+        }
+        Playlist p = findPlaylistByName(name);
+        List<Music> musicPlaylist = _JsonPathToMusicList(p.getMusicList());
+        List<Music> musicPlaylistShuffle = _JsonPathToMusicList(p.getMusicListShuffle());
+        musicPlaylist.addAll(newMusicList);
+        Collections.shuffle(newMusicList);
+        musicPlaylistShuffle.addAll(newMusicList);
+        r.beginTransaction();
+        p.setMusicList(_musicListToJsonPath(musicPlaylist));
+        p.setMusicListShuffle(_musicListToJsonPath(musicPlaylistShuffle));
+        r.commitTransaction();
+    }
+
+    private String _musicListToJsonPath(List<Music> musicList) {
         List<String> listPath = new ArrayList<>();
         for (Music m : musicList) {
             listPath.add(m.getPath());
         }
         Gson gson = new Gson();
-        String jsonPath = gson.toJson(listPath);
-        p.setMusicList(jsonPath);
-        r.commitTransaction();
-        saveMusicListShuffle(name, musicList);
+        return gson.toJson(listPath);
+    }
+
+    private List<Music> _JsonPathToMusicList(String jsonPath) {
+        List<Music> listMusic = new ArrayList<>();
+        Gson gson = new Gson();
+        List<String> musicPathList = gson.fromJson(jsonPath, new TypeToken<List<String>>() {
+        }.getType());
+        for (String path : musicPathList) {
+            MusicRepo mr = new MusicRepo(r);
+            Music m = mr.findOne(path);
+            if (m != null) {
+                listMusic.add(m);
+            }
+        }
+
+        return listMusic;
     }
 
 }
